2c34ed748963dd51e7326c982da9a3b10527bb4d
Peter <petr.obintsev@gmail.com>
2023-01-29 13:35:55 +0300

    звершает адаптивную верстку сетки и декоративных элементов

--- source/js/main-header.js
+++ source/js/main-header.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("burger").addEventListener("click", function() {
-    document.querySelector("main-header").classList.toggle("open")
+    document.querySelector("header").classList.toggle("open")
   })
 })
