7f6c4492cfd0b1d77ab078e7f1f9b4ab8bf68383
Keks <keks@htmlacademy.ru>
2023-01-30 09:07:05 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,18 @@
+let navMain = document.querySelector('.main-nav');
+let headerToggle = document.querySelector('.main-header__burger-btn');
+let mainHeader = document.querySelector('.main-header');
+
+mainHeader.classList.remove('main-header--nojs');
+mainHeader.classList.remove('main-header--opened');
+mainHeader.classList.add('main-header--closed');
+
+headerToggle.addEventListener('click', function () {
+  if (mainHeader.classList.contains('main-header--closed')) {
+    mainHeader.classList.remove('main-header--closed');
+    mainHeader.classList.add('main-header--opened');
+
+  } else {
+    mainHeader.classList.add('main-header--closed');
+    mainHeader.classList.remove('main-header--opened');
+  }
+});
