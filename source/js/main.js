5e0105664d2e212865648f0331e07aa3fcc57273
Peter <petr.obintsev@gmail.com>
2023-01-30 08:59:16 +0300

    удаляет закомментированный код в js

--- source/js/main.js
+++ source/js/main.js
@@ -16,9 +16,3 @@ headerToggle.addEventListener('click', function () {
     mainHeader.classList.remove('main-header--opened');
   }
 });
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   document.getElementById("burger").addEventListener("click", function() {
-//     document.querySelector("header").classList.toggle("open")
-//   })
-// })
